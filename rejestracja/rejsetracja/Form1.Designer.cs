28518e118fcd74d30d9089bf7baa899c6f9c5343
Dell_Precision\DellPrecision <krzbudz@o2.pl>
2020-09-24 19:19:26 +0200

    initial commit

---
+++ rejestracja/rejsetracja/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace rejsetracja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtRoomNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtReservationUntil = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtReservationFrom = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnSave.Location = new System.Drawing.Point(12, 171);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 25);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Zapisz";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Imię";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 8);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(109, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 202);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(518, 170);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.DoubleClick += new System.EventHandler(this.dataGridView_DoubleClick);
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(120, 34);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(109, 20);
+            this.txtSurname.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nazwisko";
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Location = new System.Drawing.Point(120, 60);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(109, 20);
+            this.txtMobileNumber.TabIndex = 7;
+            this.txtMobileNumber.TextChanged += new System.EventHandler(this.txtMobileNumber_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numer telefonu";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnReset.Location = new System.Drawing.Point(198, 171);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(84, 25);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Zresetuj";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnDelete.Location = new System.Drawing.Point(107, 171);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(84, 25);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtRoomNumber
+            // 
+            this.txtRoomNumber.Location = new System.Drawing.Point(120, 139);
+            this.txtRoomNumber.Name = "txtRoomNumber";
+            this.txtRoomNumber.Size = new System.Drawing.Size(109, 20);
+            this.txtRoomNumber.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Numer pokoju";
+            // 
+            // txtReservationUntil
+            // 
+            this.txtReservationUntil.Location = new System.Drawing.Point(120, 113);
+            this.txtReservationUntil.Name = "txtReservationUntil";
+            this.txtReservationUntil.Size = new System.Drawing.Size(109, 20);
+            this.txtReservationUntil.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Data rezerwacji do ";
+            // 
+            // txtReservationFrom
+            // 
+            this.txtReservationFrom.Location = new System.Drawing.Point(120, 86);
+            this.txtReservationFrom.Name = "txtReservationFrom";
+            this.txtReservationFrom.Size = new System.Drawing.Size(109, 20);
+            this.txtReservationFrom.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Data rezewacji od";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(271, 8);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(156, 20);
+            this.txtSearch.TabIndex = 16;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnSearch.Location = new System.Drawing.Point(446, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 25);
+            this.btnSearch.TabIndex = 17;
+            this.btnSearch.Text = "Szukaj";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(542, 384);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtReservationUntil);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtReservationFrom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtRoomNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Rezerwacja";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtRoomNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtReservationUntil;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtReservationFrom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ rejestracja/rejsetracja/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace rejsetracja
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\Studia\projekt programowanie\Projekt rezerwacji\baza\customers.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+        int CustomerId = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                if (btnSave.Text == "Zapisz")
+                {
+                    SqlCommand sqlCmd = new SqlCommand("ReservationAddOrEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                    sqlCmd.Parameters.AddWithValue("@CustomerID", 0);
+                    sqlCmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Surname", txtSurname.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@MobileNumber", txtMobileNumber.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Booking_from", txtReservationFrom.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Booking_until", txtReservationUntil.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@RoomNumber", txtRoomNumber.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Zapisano pomyślnie");
+                }
+                else
+                {
+                    SqlCommand sqlCmd = new SqlCommand("ReservationAddOrEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@mode", "Edit");
+                    sqlCmd.Parameters.AddWithValue("@CustomerID", CustomerId);
+                    sqlCmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Surname", txtSurname.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@MobileNumber", txtMobileNumber.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Booking_from", txtReservationFrom.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Booking_until", txtReservationUntil.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@RoomNumber", txtRoomNumber.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Zaktualizowano pomyślnie");
+                }
+                Reset();
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        void FillDataGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ReservationViewOrSearch", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@ReservationSurname", txtSearch.Text.Trim());
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            dataGridView.DataSource = dtbl;
+            dataGridView.Columns[0].Visible = false;
+            sqlCon.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Błąd");
+
+            }
+
+
+        }
+
+  
+        private void dataGridView_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView.CurrentRow.Index != -1)
+            {
+                CustomerId = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value.ToString());
+                txtName.Text = dataGridView.CurrentRow.Cells[1].Value.ToString();
+                txtSurname.Text = dataGridView.CurrentRow.Cells[2].Value.ToString();
+                txtMobileNumber.Text = dataGridView.CurrentRow.Cells[3].Value.ToString();
+                txtReservationFrom.Text = dataGridView.CurrentRow.Cells[4].Value.ToString();
+                txtReservationUntil.Text = dataGridView.CurrentRow.Cells[5].Value.ToString();
+                txtRoomNumber.Text = dataGridView.CurrentRow.Cells[6].Value.ToString();
+                btnSave.Text = "Zaktualizuj";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        void Reset()
+        {
+            txtName.Text = txtSurname.Text = txtMobileNumber.Text = txtReservationFrom.Text = 
+                txtReservationUntil.Text = txtRoomNumber.Text = "";
+            btnSave.Text = "Zapisz";
+            CustomerId = 0;
+            btnDelete.Enabled = false ;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                   if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                   SqlCommand sqlCmd = new SqlCommand("CustomerDeletion", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@CustomerId", CustomerId);
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Usunięto pomyślnie");
+                    Reset();
+                    FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Błąd");
+            }
+
+
+        }
+
+        private void txtMobileNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
